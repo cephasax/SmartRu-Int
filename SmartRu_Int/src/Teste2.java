f9196a2de27aa5257337b19a86b8ecb3ed42bfe7
Cephas Barreto <cephasax@hotmail.com>
2016-06-16 17:17:18 -0300

    funfando ate a matriz

---
+++ SmartRu_Int/src/Teste2.java
@@ -0,0 +1,27 @@
+import br.ufrn.imd.smartRu.inteligencia.InteligenceRu;
+
+public class Teste2{
+	
+	private static InteligenceRu iu;
+	
+	public static void main(String[] args) {
+		
+		int nRasps = 1;
+		int numSensores = 6;
+		
+		iu = new InteligenceRu(nRasps, numSensores);
+		String dados = new String(iu.mock());
+		iu.adicionarRaspberryFromJason(dados);
+		int[][] mat = new int[nRasps][numSensores];
+		
+		mat = iu.receberMatriz();
+		
+		for(int i = 0; i < nRasps; i++){
+			for(int j = 0; j < numSensores; j++){
+				System.out.print(mat[i][j] + "\t");
+			}
+			System.out.print("\n");
+		}
+	}
+	
+}
--- SmartRu_Int/src/br/ufrn/imd/smartRu/inteligencia/InteligenceRu.java
+++ SmartRu_Int/src/br/ufrn/imd/smartRu/inteligencia/InteligenceRu.java
@@ -1,21 +1,37 @@
 package br.ufrn.imd.smartRu.inteligencia;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import com.google.gson.Gson;
+
 import br.ufrn.imd.smartRu.modelo.FilaRu;
 import br.ufrn.imd.smartRu.modelo.Raspberry;
+import br.ufrn.imd.smartRu.modelo.Sensor;
 
 public class InteligenceRu {
 
 	private FilaRu appFila;
-	private final double VALOR_PADRAO = 30.0;
+	private final double VALOR_PADRAO = 90.0;
 	
 	public InteligenceRu(int NumeroRaspberry, int NumeroSensores){
 		this.appFila = new FilaRu(NumeroRaspberry, NumeroSensores);
 	}
 	
-	public void construirAplicacao(String dados){
-		//Parse do dado para um appFila
+	public void adicionarRaspberryFromJason(String json){
+		Raspberry rasp = loadRaspFromJSON(json);
+		if(appFila.getRaspberryNome(rasp.getIdentificador()) == null){
+			appFila.incluirRasp(rasp);
+		}
+		System.out.println("Raspberry" + rasp.getIdentificador()+ "incluido com sucesso");
 	}
 	
+ 	public Raspberry loadRaspFromJSON(String jsonString) {
+	    Gson gson = new Gson();
+	    Raspberry rasp = gson.fromJson(jsonString, Raspberry.class);
+	    return rasp;
+	}
+		
 	public void atualizarAplicacao(String dados){
 		//Parse do dado para o appFila colocando novos dados
 	}
@@ -69,4 +85,28 @@ public class InteligenceRu {
 		}
 		return matriz;
 	}
+
+	public String mock(){
+		Raspberry rasp = new Raspberry("Rasp1");
+		Random gerador = new Random();
+
+		Gson gson = new Gson();
+
+		for (int i = 0; i < 6; i++) {
+
+			Sensor sensor = new Sensor();
+			
+			double numero = gerador.nextDouble() * 100; // [0,100[
+			double numero1 = Math.round(numero * 100) / 100d;
+			System.out.println(numero1);
+			sensor.setNome("s" + i);
+			sensor.setValor(numero1);
+			rasp.adicionarSensor(sensor);
+		}
+
+		String userJSONString = gson.toJson(rasp);
+
+		return userJSONString;
+	}
+	
 }
