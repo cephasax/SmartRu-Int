202c797b2574d73239dfe7eecc0f7964fc7b95fd
andersonpablo <mib_anderson@hotmail.com>
2016-06-17 15:34:26 -0300

    verificação para valores negativos

--- SmartRu_Int/src/br/ufrn/imd/smartRu/inteligencia/InteligenceRu.java
+++ SmartRu_Int/src/br/ufrn/imd/smartRu/inteligencia/InteligenceRu.java
@@ -13,72 +13,74 @@ public class InteligenceRu {
 
 	private FilaRu appFila;
 	private final double VALOR_PADRAO = 90.0;
-	
-	public InteligenceRu(int NumeroRaspberry, int NumeroSensores){
+
+	public InteligenceRu(int NumeroRaspberry, int NumeroSensores) {
 		this.appFila = new FilaRu(NumeroRaspberry, NumeroSensores);
 	}
-	
-	public void adicionarRaspberryFromJason(String json){
+
+	public void adicionarRaspberryFromJason(String json) {
 		Raspberry rasp = loadRaspFromJSON(json);
-		if(appFila.getRaspberryNome(rasp.getIdentificador()) == null){
+		if (appFila.getRaspberryNome(rasp.getIdentificador()) == null) {
 			appFila.incluirRasp(rasp);
 		}
-		System.out.println("Raspberry" + rasp.getIdentificador()+ "incluido com sucesso");
+		System.out.println("Raspberry" + rasp.getIdentificador()
+				+ "incluido com sucesso");
 	}
-	
- 	public Raspberry loadRaspFromJSON(String jsonString) {
-	    Gson gson = new Gson();
-	    Raspberry rasp = gson.fromJson(jsonString, Raspberry.class);
-	    return rasp;
+
+	public Raspberry loadRaspFromJSON(String jsonString) {
+		Gson gson = new Gson();
+		Raspberry rasp = gson.fromJson(jsonString, Raspberry.class);
+		return rasp;
 	}
-		
-	public void atualizarAplicacao(String dados){
-		//Parse do dado para o appFila colocando novos dados
+
+	public void atualizarAplicacao(String dados) {
+		// Parse do dado para o appFila colocando novos dados
 	}
-	
-	public String VerificarRasps(){
+
+	public String VerificarRasps() {
 		StringBuilder builder = new StringBuilder();
 		builder.append("Raspberrys pertencentes ao sistema: \n");
-		for(Raspberry rasp: appFila.getRasps()){
+		for (Raspberry rasp : appFila.getRasps()) {
 			builder.append(rasp.getIdentificador());
 			builder.append("\n");
 		}
 		return builder.toString();
 	}
-	
-	public int[][] receberMatriz(){
-		int[][] matriz = new int
-				[appFila.getNumeroLinhas()][appFila.getNumeroColunas()];
-		
+
+	public int[][] receberMatriz() {
+		int[][] matriz = new int[appFila.getNumeroLinhas()][appFila
+				.getNumeroColunas()];
+
 		/**
 		 * Percorre todos os Raspberrys cadastrados por ordem de cadastro.
-		 * Inclui como sensor da fila todos os sensores do raspberry atual 
-		 * ate que nao haja mais nenhum sensor. Apos isso segue para o proximo raspberry
+		 * Inclui como sensor da fila todos os sensores do raspberry atual ate
+		 * que nao haja mais nenhum sensor. Apos isso segue para o proximo
+		 * raspberry
 		 * 
 		 * Repete o passo anterior ate que nao haja mais raspberrys no array
-		*/
-		
+		 */
+
 		double v = -1;
-		
-		for(int i = 0; i < appFila.getNumeroLinhas(); i++){
-			for(int j = 0; j < appFila.getNumeroColunas(); j++){
-				
+
+		for (int i = 0; i < appFila.getNumeroLinhas(); i++) {
+			for (int j = 0; j < appFila.getNumeroColunas(); j++) {
+
 				v = -1;
-				
-				if((i < appFila.getRasps().size()) && 
-						(j < appFila.getRasps().get(i).getSensores().size())){
-					
-					v = appFila.getRasps().get(i).getSensores().get(j).getValor();
+
+				if ((i < appFila.getRasps().size())
+						&& (j < appFila.getRasps().get(i).getSensores().size())) {
+
+					v = appFila.getRasps().get(i).getSensores().get(j)
+							.getValor();
 				}
-								
-				//verificacao do valor do sensor e insercao na matriz de inteiros
-				if( (v >= 0) && (v <= VALOR_PADRAO)){
+
+				// verificacao do valor do sensor e insercao na matriz de
+				// inteiros
+				if ((v >= 0) && (v <= VALOR_PADRAO)) {
 					matriz[i][j] = 1;
-				}
-				else if(v > VALOR_PADRAO){
+				} else if (v > VALOR_PADRAO) {
 					matriz[i][j] = 0;
-				}
-				else if(v < 0){
+				} else if (v < 0) {
 					matriz[i][j] = -1;
 				}
 			}
@@ -86,7 +88,7 @@ public class InteligenceRu {
 		return matriz;
 	}
 
-	public String mock(){
+	public String mock() {
 		Raspberry rasp = new Raspberry("Rasp1");
 		Random gerador = new Random();
 
@@ -95,7 +97,7 @@ public class InteligenceRu {
 		for (int i = 0; i < 6; i++) {
 
 			Sensor sensor = new Sensor();
-			
+
 			double numero = gerador.nextDouble() * 100; // [0,100[
 			double numero1 = Math.round(numero * 100) / 100d;
 			System.out.println(numero1);
@@ -108,20 +110,24 @@ public class InteligenceRu {
 
 		return userJSONString;
 	}
-	
-	public double percentFila(int mat[][]){
-		int linha = appFila.getNumeroLinhas(), coluna = appFila.getNumeroColunas();
+
+	public double percentFila(int mat[][]) {
+		int linha = appFila.getNumeroLinhas(), coluna = appFila
+				.getNumeroColunas();
 		double cont = 0, total = 0;
-		for(int i=0; i<linha; i++){
-			for(int j=0; j<coluna; j++){
-				if(mat[i][j]==1)
+		for (int i = 0; i < linha; i++) {
+			for (int j = 0; j < coluna; j++) {
+				if (mat[i][j] == 1) {
 					cont++;
-				
-				total++;
+				}
+				if (mat[i][j] != -1) {
+					total++;
+
+				}
 			}
+			
 		}
-		
-		return (cont/total);
-	}
+		return (cont / total) * 100;
 	
+	}
 }
