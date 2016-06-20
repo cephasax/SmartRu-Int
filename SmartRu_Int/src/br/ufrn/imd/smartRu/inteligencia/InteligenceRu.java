03ea26d9ade4f99706594fe4d31ea944b7a9b972
Cephas Barreto <cephasax@hotmail.com>
2016-06-20 17:17:28 -0300

    continuacao da inteligencia

--- SmartRu_Int/src/br/ufrn/imd/smartRu/inteligencia/InteligenceRu.java
+++ SmartRu_Int/src/br/ufrn/imd/smartRu/inteligencia/InteligenceRu.java
@@ -1,84 +1,91 @@
 package br.ufrn.imd.smartRu.inteligencia;
 
 import java.util.ArrayList;
-import java.util.Random;
 
 import com.google.gson.Gson;
 
 import br.ufrn.imd.smartRu.modelo.FilaRu;
 import br.ufrn.imd.smartRu.modelo.Raspberry;
-import br.ufrn.imd.smartRu.modelo.Sensor;
 
 public class InteligenceRu {
 
 	private FilaRu appFila;
+	private ArrayList<int[][]> historicoBases;
+
+	// Constantes
+	private Config config; // 3 linhas e 4 colunas - 20-06-2016
 	private final double VALOR_PADRAO = 90.0;
-	
-	public InteligenceRu(int NumeroRaspberry, int NumeroSensores){
-		this.appFila = new FilaRu(NumeroRaspberry, NumeroSensores);
+
+	// controles
+	private int controleMatriz = 0;
+	private ArrayList<String> nomesRasps;
+
+	public InteligenceRu() {
+		this.config = new Config();
+		this.appFila = new FilaRu(config.getNumeroLinhas(), config.getNumeroColunas());
+		historicoBases = new ArrayList<int[][]>();
+		nomesRasps = new ArrayList<String>();
 	}
-	
-	public void adicionarRaspberryFromJason(String json){
+
+	public void adicionarRaspberryFromJason(String json) {
 		Raspberry rasp = loadRaspFromJSON(json);
-		if(appFila.getRaspberryNome(rasp.getIdentificador()) == null){
+		if (appFila.getRaspberryNome(rasp.getIdentificador()) == null) {
 			appFila.incluirRasp(rasp);
+			nomesRasps.add(rasp.getIdentificador());
+			System.out.println("Raspberry " + rasp.getIdentificador() + " incluido com sucesso");
 		}
-		System.out.println("Raspberry" + rasp.getIdentificador()+ "incluido com sucesso");
 	}
-	
- 	public Raspberry loadRaspFromJSON(String jsonString) {
-	    Gson gson = new Gson();
-	    Raspberry rasp = gson.fromJson(jsonString, Raspberry.class);
-	    return rasp;
+
+	public Raspberry loadRaspFromJSON(String jsonString) {
+		Gson gson = new Gson();
+		Raspberry rasp = gson.fromJson(jsonString, Raspberry.class);
+		return rasp;
 	}
+
+	public void atualizarAplicacao(String dados) {
 		
-	public void atualizarAplicacao(String dados){
-		//Parse do dado para o appFila colocando novos dados
 	}
-	
-	public String VerificarRasps(){
+
+	public String VerificarRasps() {
 		StringBuilder builder = new StringBuilder();
 		builder.append("Raspberrys pertencentes ao sistema: \n");
-		for(Raspberry rasp: appFila.getRasps()){
-			builder.append(rasp.getIdentificador());
+		for (String nome: nomesRasps) {
+			builder.append(nome);
 			builder.append("\n");
 		}
 		return builder.toString();
 	}
-	
-	public int[][] receberMatriz(){
-		int[][] matriz = new int
-				[appFila.getNumeroLinhas()][appFila.getNumeroColunas()];
-		
+
+	public int[][] receberMatriz() {
+		int[][] matriz = new int[appFila.getNumeroLinhas()][appFila.getNumeroColunas()];
+
 		/**
 		 * Percorre todos os Raspberrys cadastrados por ordem de cadastro.
-		 * Inclui como sensor da fila todos os sensores do raspberry atual 
-		 * ate que nao haja mais nenhum sensor. Apos isso segue para o proximo raspberry
+		 * Inclui como sensor da fila todos os sensores do raspberry de acordo
+		 * com a posicao declarada no atributo base da classe Config
 		 * 
 		 * Repete o passo anterior ate que nao haja mais raspberrys no array
-		*/
-		
+		 */
+
 		double v = -1;
-		
-		for(int i = 0; i < appFila.getNumeroLinhas(); i++){
-			for(int j = 0; j < appFila.getNumeroColunas(); j++){
+
+		for (int i = 0; i < appFila.getNumeroLinhas(); i++) {
+			for (int j = 0; j < appFila.getNumeroColunas(); j++) {
 				
-				v = -1;
-				
-				if((i < appFila.getRasps().size()) && 
-						(j < appFila.getRasps().get(i).getSensores().size())){
-					
-					v = appFila.getRasps().get(i).getSensores().get(j).getValor();
-				}
-								
-				//verificacao do valor do sensor e insercao na matriz de inteiros
-				if( (v >= 0) && (v <= VALOR_PADRAO)){
+				String registro = new String(config.base[i][j]);
+				String nomeRasp = new String(registro.substring(0, 1));
+				String nomeSensor = new String(registro.substring(3, 4));;
+			
+				v = appFila.getRaspberryNome(nomeRasp).getSensorNome(nomeSensor).getValor();				
+				// verificacao do valor do sensor e insercao na matriz de
+				// inteiros
+				if ((v >= 0) && (v <= VALOR_PADRAO)) {
 					matriz[i][j] = 1;
 				}
-				else if(v > VALOR_PADRAO){
+				else if (v > VALOR_PADRAO) {
 					matriz[i][j] = 0;
 				}
-				else if(v < 0){
+				else if (v < 0) {
 					matriz[i][j] = -1;
 				}
 			}
@@ -86,42 +93,39 @@ public class InteligenceRu {
 		return matriz;
 	}
 
-	/*public String mock(){
-		Raspberry rasp = new Raspberry("Rasp1");
-		Random gerador = new Random();
+	/*
+	 * public String mock(){ Raspberry rasp = new Raspberry("Rasp1"); Random
+	 * gerador = new Random();
+	 * 
+	 * Gson gson = new Gson();
+	 * 
+	 * for (int i = 0; i < 6; i++) {
+	 * 
+	 * Sensor sensor = new Sensor();
+	 * 
+	 * double numero = gerador.nextDouble() * 100; // [0,100[ double numero1 =
+	 * Math.round(numero * 100) / 100d; System.out.println(numero1);
+	 * sensor.setNome("s" + i); sensor.setValor(numero1);
+	 * rasp.adicionarSensor(sensor); }
+	 * 
+	 * String userJSONString = gson.toJson(rasp);
+	 * 
+	 * return userJSONString; }
+	 */
 
-		Gson gson = new Gson();
-
-		for (int i = 0; i < 6; i++) {
-
-			Sensor sensor = new Sensor();
-			
-			double numero = gerador.nextDouble() * 100; // [0,100[
-			double numero1 = Math.round(numero * 100) / 100d;
-			System.out.println(numero1);
-			sensor.setNome("s" + i);
-			sensor.setValor(numero1);
-			rasp.adicionarSensor(sensor);
-		}
-
-		String userJSONString = gson.toJson(rasp);
-
-		return userJSONString;
-	}*/
-	
-	public double percentFila(int mat[][]){
+	public double percentFila(int mat[][]) {
 		int linha = appFila.getNumeroLinhas(), coluna = appFila.getNumeroColunas();
 		double cont = 0, total = 0;
-		for(int i=0; i<linha; i++){
-			for(int j=0; j<coluna; j++){
-				if(mat[i][j]==1)
+		for (int i = 0; i < linha; i++) {
+			for (int j = 0; j < coluna; j++) {
+				if (mat[i][j] == 1)
 					cont++;
-				
+
 				total++;
 			}
 		}
-		
-		return (cont/total);
+
+		return (cont / total);
 	}
-	
+
 }
