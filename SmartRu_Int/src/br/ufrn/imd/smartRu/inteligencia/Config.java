35750ae423902d7b03adda61e305d723f1ac0003
Cephas Barreto <cephasax@hotmail.com>
2016-06-20 16:33:16 -0300

    adicionado parametro de configuracao na classe config.java

--- SmartRu_Int/src/br/ufrn/imd/smartRu/inteligencia/Config.java
+++ SmartRu_Int/src/br/ufrn/imd/smartRu/inteligencia/Config.java
@@ -9,8 +9,19 @@ import com.google.gson.Gson;
 
 public class Config {
 
+	private int numeroLinhas = 3;
+	private int numeroColunas = 4;
+	private String base[][];
+
+	public Config() {
+		base = new String[][] { 
+			{ "R1-S1", "R1-S2", "R2-S1", "R2-S2" }, 
+			{ "R1-S3", "R1-S4", "R2-S3", "R2-S4" },
+			{ "R1-S5", "R1-S6", "R2-S5", "R2-S6" } };
+	}
+
 	public String mock(String nome) {
-		
+
 		Raspberry rasp = new Raspberry(nome);
 		Random gerador = new Random();
 
@@ -31,6 +42,30 @@ public class Config {
 		String userJSONString = gson.toJson(rasp);
 
 		return userJSONString;
+	}
+
+	public int getNumeroLinhas() {
+		return numeroLinhas;
+	}
+
+	public void setNumeroLinhas(int numeroLinhas) {
+		this.numeroLinhas = numeroLinhas;
+	}
+
+	public int getNumeroColunas() {
+		return numeroColunas;
+	}
+
+	public void setNumeroColunas(int numeroColunas) {
+		this.numeroColunas = numeroColunas;
+	}
+
+	public String[][] getBase() {
+		return base;
+	}
+
+	public void setBase(String[][] base) {
+		this.base = base;
 	}
 
 }
